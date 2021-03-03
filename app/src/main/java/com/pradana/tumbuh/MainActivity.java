9ae224a3a6be8e0a5ef60cf5ba6ed2c968758e2f
Pradana Setialana <pradana.setialana@gmail.com>
2021-03-03 08:50:17 +0700

    Berpindah ke activity lain ketika button diclick

--- app/src/main/java/com/pradana/tumbuh/MainActivity.java
+++ app/src/main/java/com/pradana/tumbuh/MainActivity.java
@@ -1,14 +1,39 @@
 package com.pradana.tumbuh;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView);
+    }
+
+    public void tambahData(View view) {
+        Intent i = new Intent(this, TambahEditActivity.class);
+        startActivityForResult(i, getResources().getInteger(R.integer.INTENT_TAMBAH));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == getResources().getInteger(R.integer.INTENT_TAMBAH)) {
+            if (resultCode == Activity.RESULT_OK) {
+                text.setText(data.getStringExtra("deskripsi"));
+            }
+        }
     }
 }
--- app/src/main/java/com/pradana/tumbuh/TambahEditActivity.java
+++ app/src/main/java/com/pradana/tumbuh/TambahEditActivity.java
@@ -2,13 +2,29 @@ package com.pradana.tumbuh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class TambahEditActivity extends AppCompatActivity {
+
+    EditText editDeskripsi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_edit);
+
+        editDeskripsi = findViewById(R.id.editTextTextMultiLine);
+    }
+
+    public void save(View view) {
+        String deskripsi = editDeskripsi.getText().toString();
+        Intent i = new Intent();
+        i.putExtra("deskripsi", deskripsi);
+        setResult(Activity.RESULT_OK, i);
+        finish();
     }
 }
